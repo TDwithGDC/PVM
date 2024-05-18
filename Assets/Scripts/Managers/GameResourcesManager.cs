355852b6c5c552a161770abec7bd8173885ac761
imtdcat <98376382+imtdcat@users.noreply.github.com>
2024-05-18 17:18:05 +0800

    初始化
    初始化库

---
+++ Assets/Scripts/Managers/GameResourcesManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 管理游戏中玩家的资源（煤炭，铁。。。。）
+/// 并充当游戏商店
+/// </summary>
+
+public class GameResourcesManager : MonoBehaviour
+{
+    public float iron; //铁
+    public float coal; //煤炭
+    public float water; //水
+    public float log; //原木
+    public float stone; //石头
+    public float food; //食物
+    public float power; //电力
+    public float steel; //钢
+    public float wood; //木材
+    public float money;
+}
---
+++ Assets/Scripts/Other/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform cameraTrans;
+    public float standardSpeed;
+    public float zoomSpeed;
+
+    private void Awake()
+    {
+        cameraTrans = transform;
+    }
+
+    private void Update()
+    {
+        // 相机缩放
+        float m = -Input.GetAxis("Mouse ScrollWheel");
+        cameraTrans.Translate(new Vector3(0, m * zoomSpeed, 0) * Time.deltaTime, Space.World);
+        
+        // 相机移动
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 v3 = new Vector3(h, 0, v);
+        v3 *= Input.GetKey(KeyCode.LeftControl) ? (standardSpeed * 2) : standardSpeed;
+        cameraTrans.Translate(v3 * Time.deltaTime, Space.World);
+    }
+}
